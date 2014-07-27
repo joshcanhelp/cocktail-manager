0d58ee7213df6c2112e71915b63d48c3e4267181
Josh Cunningham <josh@joshcanhelp.com>
2014-07-27 14:36:17 -0700

    DB sonfig changes to include OpenShift

--- config/db.js
+++ config/db.js
@@ -3,21 +3,21 @@
 var mongoose = require('mongoose');
 var nodeEnv = process.env.NODE_ENV || 'production';
 
-// Set DB URI based on NODE_ENV
-var dbLocations = {
-	production : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/cocktail-manager',
-	development: 'mongodb://localhost/cocktail-manager',
-	test       : 'mongodb://localhost/cocktail-manager-test'
-};
+var dbName = nodeEnv === 'test' ? 'cocktail-manager-test' : 'cocktail-manager';
+var dbUrl = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.OPENSHIFT_MONGODB_DB_URL;
+
+if (!dbUrl) {
+	dbUrl = 'mongodb://localhost/';
+}
 
 // Connect when this file is required
-module.exports = mongoose.connect(dbLocations[nodeEnv], function (err) {
+module.exports = mongoose.connect(dbUrl + dbName, function (err) {
 	"use strict";
 
 	if (err) {
-		console.log('ERROR connecting to: ' + dbLocations[nodeEnv] + '. ' + err);
+		console.log('ERROR connecting to: ' + dbUrl + dbName + '. ' + err);
 	} else {
-		console.log('Succeeded connecting to: ' + dbLocations[nodeEnv]);
+		console.log('Succeeded connecting to: ' + dbUrl + dbName);
 	}
 
 });
